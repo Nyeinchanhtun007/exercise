596ef240246fba1c146ee8599a472c729f5dc6b5
Nyeinchanhtun007 <rainestarboylay@gmail.com>
2023-11-28 23:03:07 +0630

    exercise

---
+++ condition.js
@@ -0,0 +1,18 @@
+// let a = prompt("Enter first Number");
+// let b = prompt ("Enter last Number");
+
+
+
+// if(a+b >= 0 && a+b < 20){
+//   alert("The number is between 0 and 20");
+// }else if(a+b >= 20 && a+b < 40 ){
+//   alert("This number is between 20 and 40");
+// }else if(a+b >= 40 && a+b < 60 ){
+//   alert("This number is between 40 and 60");
+// }else if(a+b >=60 && a+b < 80){
+//   alert("This number is between 60 and 80");
+// }else if (a+b >= 80 && a+b < 100 ){
+//   alert("This number is between 80 and 100")
+// }else {
+//   alert("This number is greater than 100");
+// }
